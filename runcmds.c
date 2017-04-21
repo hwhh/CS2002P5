88b25fb8867aa3caee81e226134706b135f6b98b
hwhh <hwhh@st-andrews.ac.uk>
2017-04-21 12:34:24 +0100

    Fixed everything moving on

--- runcmds.c
+++ runcmds.c
@@ -11,7 +11,6 @@ void runcmds(Command commands){
 //    strcat(cwd,"/");
 //    strcat(cwd, commands.prog_args[0]);
     //char *cwd = "/Users/henryhargreaves/Documents/University/Year_2/CS2002/Practicals/CS2002P5/Test/print_args.sh";
-
     const char *cwd = commands.prog_args[0];
     char *args[commands.total];
     args[0] = commands.prog_args[0];
@@ -19,10 +18,8 @@ void runcmds(Command commands){
         args[i] = commands.prog_args[i];
     }
     args[commands.total] = NULL;
-
     char path[1024] = "/Users/henryhargreaves/Documents/University/Year_2/CS2002/Practicals/CS2002P5/Practical-SP/";
     if (commands.write_c == 1){
-        commands.write_c = 0;
         char *dir = strcat(path, commands.write);
         int file = open(dir,O_CREAT|O_WRONLY|O_APPEND,S_IRWXU);
         if(file < 0) {
@@ -33,7 +30,6 @@ void runcmds(Command commands){
         execv(cwd, args);
         close(file);
     }if (commands.read_c == 1) {
-        commands.read_c = 0;
         char *dir = strcat(path, commands.read);
         int file = open(dir,O_RDONLY);
         if (file < 0) {
@@ -52,8 +48,6 @@ void runcmds(Command commands){
     free(commands.prog_args);
 }
 
-
-
 int main() {
     char **lines = (char**) malloc(sizeof(char*)*1024);
     for(int i=0; i<1024; i++) { lines[i] = (char*)malloc(sizeof(char)*1024); }
@@ -64,61 +58,18 @@ int main() {
         strcpy(lines[count++], line);
     }
     for (int i = 0; i < count; i++) {
-        int child = fork();
-        if (child != 0) {/* Parent */
-//            int stat_loc;
-//            pid_t cpid = wait (&stat_loc);
+        int cpid = fork();
+        if(cpid<0){ /* handle error */}
+        if (cpid != 0) {/* Parent */
+            int stat_loc;
+            (void) wait (&stat_loc) ;
+
         } else { /* Child */
             runcmds(parse_string(lines[i]));
             exit(78);
         }
-
-
-
-
-
-//        int child = fork();
-//        if (child != 0) { /* Parent */
-//            int stat_loc;
-//            pid_t cpid = wait (&stat_loc) ;
-//            WEXITSTATUS(stat_loc);
-//
-//        } else { /* Child */
-//
-//            runcmds(parse_string(lines[i]));
-//        }
-
-//        int cpid = fork();
-//        if(cpid<0){ /* handle error */}
-//
-//        if (cpid != 0) {/* Parent */
-//            int stat_loc;
-//            (void) wait (&stat_loc) ;
-//
-//        } else { /* Child */
-//            runcmds(parse_string(lines[i]));
-//        }
     }
 
 
 }
-
-
-
-//        int child = fork();
-//        if (child != 0) { /* Parent */
-//            int stat_loc;
-//            (void) wait (&stat_loc) ;
-//        }
-//        else { /* Child */
-//            runcmds(parse_string(lines[i]));
-//        };
-
-
-
-
-
-
-
-
 
